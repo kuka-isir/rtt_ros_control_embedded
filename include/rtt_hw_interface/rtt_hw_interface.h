464bb9b4a99c54931f53033d6d46ea6f840f7dd6
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-02-12 15:01:57 +0100

    update header
    @jimmydasilva we should do the same everywhere

--- include/rtt_hw_interface/rtt_hw_interface.h
+++ include/rtt_hw_interface/rtt_hw_interface.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Copyright (C) 2014, Stefan Kohlbrecher, TU Darmstadt
+// Copyright (C) 2017, Antoine Hoarau, Jimmy Da Silva, ISIR-UPMC
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
